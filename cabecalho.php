8f812d2af175f9671ef5070c08c9c493eebfea75
guilhermefmr <guilherme@entersistemas.com.br>
2020-01-25 19:59:52 -0300

    Projeto de site de modas

---
+++ cabecalho.php
@@ -0,0 +1,18 @@
+
+    <header class="container">
+            <!--[if	lt	IE	9]>				<script	src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>		<![endif]--> 
+            <h1><img src="img/logo.png" alt="Logo da Mirror Fashion"></h1>
+            <p class="sacola">
+            Nenhum Item na Sacola de Compras
+            </p>
+            
+            <nav class="menu-opcoes">
+                <ul>
+                    <li><a href="#">Sua Conta</a></li>
+                    <li><a href="#">Lista de Desejos</a></li>
+                    <li><a href="#">Cartão Fidelidade</a></li>
+                    <li><a href="http://192.168.1.119/guilherme/sitemodas/mirrorfashion/sobre.html">Sobre</a></li>
+                    <li><a href="#">Ajuda</a></li>
+                </ul>
+            </nav>
+        </header>
---
+++ produto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans|Bad+Script' rel="stylesheet">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <meta name="viewport" content="width=device-width">
+        <title>Mirror Fashion</title>
+    </head>
+    <body>
+    <?php include("cabecalho.php"); ?>
+
+    <?php include("rodape.php");?>
+    </body>
+</html>
